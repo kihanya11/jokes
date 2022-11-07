5daa2f6981af730a8aa255648064f8f92debad69
kihanya11 <kihanya01@gmail.com>
2022-11-07 18:10:48 +0300

    first commit

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+public function login(){
+return view("auth.login");
+}
+public function registration(){
+    return view("auth.registration");
+}
+public function registerUser(Request $request){
+$request->validate([
+    'name'=>'required',
+    'email'=>'required',
+    'password'=>'required'
+
+]);
+
+}
+}
+
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom Authentication</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4" style="margin: top 20px;">
+                <h4>Login</h4>
+                <hr>
+                <form>
+
+                    
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" placeholder="Enter Email" name="email" value="">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="name">Password</label>
+                        <input type="password" class="form-control" placeholder="Enter Password" name="password" value="">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <button class="btn btn-block btn-primary" type="submit">Login</button>
+                    </div>
+                    <br>
+                    <a href="registration">New User !! Regsister Here</a>
+                    
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
